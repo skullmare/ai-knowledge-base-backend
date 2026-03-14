c71d0b656caf77354425798376a7bacc92bf0482
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-03-14 10:01:13 +0300

    update structure to services

--- src/services/openrouter/getEmbeddings.js
+++ src/services/openrouter/getEmbeddings.js
@@ -5,7 +5,7 @@ const openrouter = new OpenRouter({ apiKey: OPENROUTER_API_KEY });
 async function getEmbeddings(chunks, model) {
     const response = await openrouter.embeddings.generate({
         requestBody: {
-            model: model, // openai/text-embedding-3-small
+            model: model,
             input: chunks,
             encodingFormat: "float"
         }
