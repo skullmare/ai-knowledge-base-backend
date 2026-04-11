04ef83a16cccabdfd24ff9ea87bef0d841f82e1b
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-04-12 02:17:16 +0300

    update qrdrant config

--- config/qdrant.js
+++ config/qdrant.js
@@ -3,7 +3,7 @@ const logger = require('../src/utils/logger');
 
 const qdrantClient = new QdrantClient({
     url: process.env.QDRANT_URL,
-    // apiKey: process.env.QDRANT_API_KEY
+    apiKey: process.env.QDRANT_API_KEY
 });
 
 logger.success(`Векторная база данных Qdrant инициализирована (адрес сервера: ${process.env.QDRANT_URL})`);
