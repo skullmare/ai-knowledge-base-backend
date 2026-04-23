b14a364fef6e9f73542a897d76e1f1ea24d8bedc
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-04-24 00:09:20 +0300

    add rateLimit in auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { login, refresh, logout, verifyTwoFactor } = require('../controllers/auth/export');
 const validate = require('../middlewares/validate');
 const { verifyTwoFactorSchema } = require('../schemas/auth');
+const rateLimit = require('../middlewares/rateLimit');
 
-router.post('/login', login);
+router.post('/login', rateLimit({ windowMs: 15 * 60 * 1000, max: 5, message: 'Слишком много запросов на авторизацию, попробуйте позже' }), login);
 router.post('/verify-2fa', validate(verifyTwoFactorSchema), verifyTwoFactor);
 router.post('/refresh', refresh);
 router.post('/logout', logout);
