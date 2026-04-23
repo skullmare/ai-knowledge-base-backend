226e122b7a22c3c7085bb9df60586193240d50f7
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-04-24 00:27:39 +0300

    update rateLimit

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -4,11 +4,11 @@ const errorHandler = require('../utils/error-handler');
 const createRateLimit = ({
     windowMs = 15 * 60 * 1000,
     max = 10,
-    messageTemplate = 'Слишком много запросов с {ip}, попробуйте позже'
+    messageTemplate = 'Слишком много запросов, попробуйте позже'
 } = {}) => rateLimit({
     windowMs,
     limit: max,
-    standardHeaders: 'draft-7',  // для 8.x версии
+    standardHeaders: 'draft-7',
     legacyHeaders: false,
     handler: (req, res) => {
         const retryAfterSec = Math.ceil((req.rateLimit.resetTime - Date.now()) / 1000);
@@ -16,16 +16,10 @@ const createRateLimit = ({
             ? `Попробуйте через ${retryAfterSec} сек.`
             : `Попробуйте через ${Math.ceil(retryAfterSec / 60)} мин.`;
         
-        // Получаем реальный IP клиента
-        const clientIp = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
-        
-        // Подставляем IP в сообщение
-        const finalMessage = messageTemplate.replace('{ip}', clientIp);
-        
         return errorHandler(
             res,
             429,
-            finalMessage,
+            messageTemplate,
             [{ path: 'rateLimit', message: retryMessage }]
         );
     }
